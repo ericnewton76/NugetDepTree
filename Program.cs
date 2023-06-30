55398b9de55349bf15c20d185b586c2fdb04189d
Eric Newton <eric.newton@centralsquare.com>
2023-06-30 12:44:57 -0400

    commit visual studio generated editorconfig and reformat spaces to tabs

--- Program.cs
+++ Program.cs
@@ -11,168 +11,166 @@ using System.Threading.Tasks;
 
 namespace NugetDepTree
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var parser = new CommandLine.Parser((settings) =>
-            {
-                settings.IgnoreUnknownArguments = true;
-            });
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var parser = new CommandLine.Parser((settings) => {
+				settings.IgnoreUnknownArguments = true;
+			});
 
-            parser.ParseArguments<ProgramOptions>(args)
-                .WithParsed((options) => Run(options))
-                .WithNotParsed((errors) =>
-                {
-                    foreach (var error in errors)
-                        Console.WriteLine("Error: {0} {1}", error.Tag, error.StopsProcessing);
-                });
+			parser.ParseArguments<ProgramOptions>(args)
+				.WithParsed((options) => Run(options))
+				.WithNotParsed((errors) => {
+					foreach(var error in errors)
+						Console.WriteLine("Error: {0} {1}", error.Tag, error.StopsProcessing);
+				});
 
-            ProgramEnd.Wait(10);
-         }
+			ProgramEnd.Wait(10);
+		}
 
-        static void Run(ProgramOptions options)
-        {
-            Program.options = options;
-            ValidateOptions(options);
+		static void Run(ProgramOptions options)
+		{
+			Program.options = options;
+			ValidateOptions(options);
 
-            string repoFolder = GetValidPackagesPath(options.RepoPath);
+			string repoFolder = GetValidPackagesPath(options.RepoPath);
 
-            tryagain:
-            var repo = new LocalPackageRepository(repoFolder);
-            IQueryable<IPackage> packages = repo.GetPackages();
-            if (packages.Count() == 0 && repoFolder.EndsWith("packages") == false)
-            {
-                repoFolder = Path.Combine(repoFolder, "packages");
-                goto tryagain;
-            }
+		tryagain:
+			var repo = new LocalPackageRepository(repoFolder);
+			IQueryable<IPackage> packages = repo.GetPackages();
+			if(packages.Count() == 0 && repoFolder.EndsWith("packages") == false)
+			{
+				repoFolder = Path.Combine(repoFolder, "packages");
+				goto tryagain;
+			}
 
-            OutputGraph(repo, packages, 0);
-        }
+			OutputGraph(repo, packages, 0);
+		}
 
-        static void ValidateOptions(ProgramOptions options)
-        {
-            //reset depth if anything less than -1
-            if (options.Depth < -1) options.Depth = -1;
+		static void ValidateOptions(ProgramOptions options)
+		{
+			//reset depth if anything less than -1
+			if(options.Depth < -1) options.Depth = -1;
 
-            if (options.DepthSpaces < 1) options.DepthSpaces = 1;
-        }
+			if(options.DepthSpaces < 1) options.DepthSpaces = 1;
+		}
 
-        private static ProgramOptions options;
+		private static ProgramOptions options;
 
-        private static string GetValidPackagesPath(string startingPath)
-        {
-            string specifiedFolder = startingPath ?? "packages";
+		private static string GetValidPackagesPath(string startingPath)
+		{
+			string specifiedFolder = startingPath ?? "packages";
 
-            while(Directory.Exists(specifiedFolder) == false && Directory.Exists(Path.Combine(specifiedFolder,"packages"))==false)
-            {
-                Console.WriteLine("Directory '{0}' doesnt exist.", Path.GetFullPath(specifiedFolder));
-                Console.Write("Enter path for the 'packages' folder: ");
-                specifiedFolder = Console.ReadLine();
-            }
+			while(Directory.Exists(specifiedFolder) == false && Directory.Exists(Path.Combine(specifiedFolder, "packages")) == false)
+			{
+				Console.WriteLine("Directory '{0}' doesnt exist.", Path.GetFullPath(specifiedFolder));
+				Console.Write("Enter path for the 'packages' folder: ");
+				specifiedFolder = Console.ReadLine();
+			}
 
-            return specifiedFolder;
-        }
+			return specifiedFolder;
+		}
 
 
-        private static PackageComparer __comparer = new PackageComparer();
-        private static string[] _corepackages = GenerateCorePackageList();
+		private static PackageComparer __comparer = new PackageComparer();
+		private static string[] _corepackages = GenerateCorePackageList();
 
-        static void OutputGraph(LocalPackageRepository repository, IEnumerable<IPackage> packages, int depth)
-        {
-            foreach (IPackage package in packages)
-            {
-                if (_corepackages.Contains(package.Id)) continue;
+		static void OutputGraph(LocalPackageRepository repository, IEnumerable<IPackage> packages, int depth)
+		{
+			foreach(IPackage package in packages)
+			{
+				if(_corepackages.Contains(package.Id)) continue;
 
-                //write package info to output
-                WritePackageInfo(depth, package);
+				//write package info to output
+				WritePackageInfo(depth, package);
 
-                IList<IPackage> dependentPackages = new List<IPackage>();
-                foreach (var dependencySet in package.DependencySets)
-                {
-                    foreach (var dependency in dependencySet.Dependencies)
-                    {
-                        var dependentPackage = repository.FindPackage(dependency.Id, dependency.VersionSpec, true, true);
-                        if (dependentPackage != null)
-                        {
-                            //every package depends on System.Runtime
-                            if(dependentPackage.Id != "System.Runtime")
-                                dependentPackages.Add(dependentPackage);
-                        }
-                    }
-                }
+				IList<IPackage> dependentPackages = new List<IPackage>();
+				foreach(var dependencySet in package.DependencySets)
+				{
+					foreach(var dependency in dependencySet.Dependencies)
+					{
+						var dependentPackage = repository.FindPackage(dependency.Id, dependency.VersionSpec, true, true);
+						if(dependentPackage != null)
+						{
+							//every package depends on System.Runtime
+							if(dependentPackage.Id != "System.Runtime")
+								dependentPackages.Add(dependentPackage);
+						}
+					}
+				}
 
-                bool godeeper = true;
+				bool godeeper = true;
 
-                if (package.Id.StartsWith("System.") == true && options.SystemDependencies == false)
-                {
-                    godeeper = false;
-                }
+				if(package.Id.StartsWith("System.") == true && options.SystemDependencies == false)
+				{
+					godeeper = false;
+				}
 
-                if (depth + 1 <= options.Depth)
-                {
-                    godeeper = false;
-                }
-                
-                if(godeeper)
-                {
-                    OutputGraph(repository, dependentPackages.Distinct(__comparer), depth + 1);
-                }
-            }
-        }
+				if(depth + 1 <= options.Depth)
+				{
+					godeeper = false;
+				}
+
+				if(godeeper)
+				{
+					OutputGraph(repository, dependentPackages.Distinct(__comparer), depth + 1);
+				}
+			}
+		}
 
 
-        private static Dictionary<int,string> S_depthStrings = new Dictionary<int, string>();
+		private static Dictionary<int, string> S_depthStrings = new Dictionary<int, string>();
 
-        /// <summary>
-        /// Writes the package information to output
-        /// </summary>
-        /// <param name="depth"></param>
-        /// <param name="depthStrings"></param>
-        /// <param name="package"></param>
-        private static void WritePackageInfo(int depth, IPackage package)
-        {
-            string depthStr;
-            if (S_depthStrings.TryGetValue(depth, out depthStr) == false)
-            {
-                switch (options.DepthStyle)
-                {
-                    case DepthStyle.Spaces:
-                        depthStr = new string(' ', depth * options.DepthSpaces);
-                        break;
-                    case DepthStyle.Tabs:
-                        depthStr = new string('\t', depth);
-                        break;
-                    case DepthStyle.Graph:
-                        var sb = new StringBuilder(new string('-', depth * 2));
-                        if (depth > 0) sb[0] = '\\';
-                        depthStr = sb.ToString();
-                        break;
-                }
+		/// <summary>
+		/// Writes the package information to output
+		/// </summary>
+		/// <param name="depth"></param>
+		/// <param name="depthStrings"></param>
+		/// <param name="package"></param>
+		private static void WritePackageInfo(int depth, IPackage package)
+		{
+			string depthStr;
+			if(S_depthStrings.TryGetValue(depth, out depthStr) == false)
+			{
+				switch(options.DepthStyle)
+				{
+					case DepthStyle.Spaces:
+						depthStr = new string(' ', depth * options.DepthSpaces);
+						break;
+					case DepthStyle.Tabs:
+						depthStr = new string('\t', depth);
+						break;
+					case DepthStyle.Graph:
+						var sb = new StringBuilder(new string('-', depth * 2));
+						if(depth > 0) sb[0] = '\\';
+						depthStr = sb.ToString();
+						break;
+				}
 
-                S_depthStrings[depth] = depthStr;
-            }
+				S_depthStrings[depth] = depthStr;
+			}
 
-            if (options.HideVersions)
-            {
-                Console.WriteLine("{0}{1}", depthStr, package.Id, package.Version);
-                return;
-            }
-            
-            Console.Write("{0}{1} ", depthStr, package.Id, package.Version);
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("{0}", package.Version);
-            Console.ResetColor();
+			if(options.HideVersions)
+			{
+				Console.WriteLine("{0}{1}", depthStr, package.Id, package.Version);
+				return;
+			}
 
-        }
+			Console.Write("{0}{1} ", depthStr, package.Id, package.Version);
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WriteLine("{0}", package.Version);
+			Console.ResetColor();
 
-        private static string[] GenerateCorePackageList()
-        {
-            return new string[]
-            {
-                "System.Runtime",
-                "System.Reactive"
-            };
-        }
-    }
+		}
+
+		private static string[] GenerateCorePackageList()
+		{
+			return new string[]
+			{
+				"System.Runtime",
+				"System.Reactive"
+			};
+		}
+	}
 }
--- ProgramOptions.cs
+++ ProgramOptions.cs
@@ -2,33 +2,33 @@
 
 namespace NugetDepTree
 {
-    internal enum DepthStyle
-    {
-        Unspecified = 0,
-        Spaces = 1,
-        Tabs = 2,
-        Graph = 3
-    }
+	internal enum DepthStyle
+	{
+		Unspecified = 0,
+		Spaces = 1,
+		Tabs = 2,
+		Graph = 3
+	}
 
-    internal class ProgramOptions
-    {
+	internal class ProgramOptions
+	{
 
-        [Option('s', "depthStyle", Default = DepthStyle.Spaces, HelpText = "style of output when writing level of depth of the depencies.  Options are Spaces, Tabs, Graph")]
-        public DepthStyle DepthStyle { get; set; }
+		[Option('s', "depthStyle", Default = DepthStyle.Spaces, HelpText = "style of output when writing level of depth of the depencies.  Options are Spaces, Tabs, Graph")]
+		public DepthStyle DepthStyle { get; set; }
 
-        [Option("spaces", Default=2, HelpText = "Number of spaces to output when DepthStyle=Spaces.  Minimum is 1.")]
-        public int DepthSpaces { get; set; }
+		[Option("spaces", Default = 2, HelpText = "Number of spaces to output when DepthStyle=Spaces.  Minimum is 1.")]
+		public int DepthSpaces { get; set; }
 
-        [Option('d', "max-depth", Default=-1, HelpText="print package dependencies up to a specified depth. N=0 shows only direct dependencies.  default is -1 (full depth)")]
-        public int Depth { get; set; }
+		[Option('d', "max-depth", Default = -1, HelpText = "print package dependencies up to a specified depth. N=0 shows only direct dependencies.  default is -1 (full depth)")]
+		public int Depth { get; set; }
 
-        [Option("system", Default = false, HelpText = "Dont exclude System.* dependencies.  Default is false (ie doesnt follow System.* dependencies)")]
-        public bool SystemDependencies { get; set; }
+		[Option("system", Default = false, HelpText = "Dont exclude System.* dependencies.  Default is false (ie doesnt follow System.* dependencies)")]
+		public bool SystemDependencies { get; set; }
 
-        [Value(0)]
-        public string RepoPath { get; set; }
+		[Value(0)]
+		public string RepoPath { get; set; }
 
-        [Option("hide-versions", Default=false, HelpText="hides the package version information")]
-        public bool HideVersions { get; set; }
-    }
+		[Option("hide-versions", Default = false, HelpText = "hides the package version information")]
+		public bool HideVersions { get; set; }
+	}
 }
